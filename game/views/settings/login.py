feeb75f49e042095a27b29607b2d21a0ab726756
2179837317@qq.com <2179837317@qq.com>
2023-07-28 11:41:29 +0800

    normalizing unit

--- game/views/settings/login.py
+++ game/views/settings/login.py
@@ -15,3 +15,6 @@ def signin(request):
     return JsonResponse({
         'result': "success"
     })
+
+
+
--- static/js/src/palyground/game_map/zbase.js
+++ static/js/src/palyground/game_map/zbase.js
@@ -1,4 +1,4 @@
-class GameMap extends AcGameObject {
+ class GameMap extends AcGameObject {
     constructor(playground) {
         super();
         this.playground = playground;
@@ -10,6 +10,13 @@ class GameMap extends AcGameObject {
     }
 
     start() {
+    }
+
+    resize() {
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.ctx.fillStyle = "rgba(0, 0, 0, 1 )";
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
 
     update() {
